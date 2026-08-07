d12dcb76c83ed40f1809c33f7167f1a63b80d19e
jharodiv <jharold.centino@gmail.com>
2026-08-07 20:52:37 +0800

    authentication can now identify if its a user or an admin

--- src/services/authentication/auth.service.ts
+++ src/services/authentication/auth.service.ts
@@ -3,11 +3,18 @@ import {
     signOut,
 } from "firebase/auth";
 
+import {
+    doc,
+    getDoc,
+} from "firebase/firestore";
+
+
 import type { AuthError } from "firebase/auth";
 
-import { auth } from "@service/database/firebase";
+import { auth, db } from "@service/database/firebase";
 
 import { getAuthErrorMessage } from "@errors/authentication/auth.error";
+import type { UserModel } from "src/models/authentication/user.model";
 
 export async function login(email: string, password: string) {
     try {
@@ -17,8 +24,29 @@ export async function login(email: string, password: string) {
             password
         );
 
+        const uid = credential.user.uid;
+
+        const userRef = doc(db, "users", uid);
+        const userSnap = await getDoc(userRef);
+
+        if (!userSnap.exists()) {
+            await signOut(auth);
+            throw new Error("User profile not found");
+        }
+
+        const user = userSnap.data() as UserModel;
+
+        if (user.role !== "admin") {
+            await signOut(auth);
+            throw new Error("Only admitrators can access this application");
+        }
+
         return credential.user;
     } catch (error) {
+        if (error instanceof Error) {
+            throw error;
+        }
+
         throw new Error(getAuthErrorMessage(error as AuthError));
     }
 }
